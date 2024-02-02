2646c68831858287c50997ffed5a412627c93cad
Shinryo <157136484+5h1nry0@users.noreply.github.com>
2024-02-02 17:16:51 -0500

    Add function to play a single round Function has two parameters: the playerSelection and the computerSelection.

--- script.js
+++ script.js
@@ -15,3 +15,38 @@ function getComputerChoice() {
     return choice;
 }
 
+function playRound(playerSelection, computerSelection) {
+    let pSelect = playerSelection.toLowerCase;
+    
+    if (playerSelection == 'rock' 
+    && computerSelection == 'paper') {
+        message = "You Lose! Paper beats Rock";
+    }
+    else if (playerSelection == 'rock' 
+    && computerSelection == 'scissors') {
+        message = "You Win! Rock beats Scissors";
+    } 
+    else if (playerSelection == 'paper' 
+    && computerSelection == 'scissors') {
+        message = "You Lose! Scissors beats Paper";
+    }
+    else if (playerSelection == 'paper' 
+    && computerSelection == 'rock') {
+        message = "You Win! Paper beats Rock";
+    } 
+    else if (playerSelection == 'scissors' 
+    && computerSelection == 'rock') {
+        message = "You Lose! Rock beats Scissors";
+    }
+    else if (playerSelection == 'scissors' 
+    && computerSelection == 'paper') {
+        message = "You Win! Scissors beats Paper";
+    }
+    else message = "It's a tie"
+    return message
+}
+const playerSelection = "scissors";
+const computerSelection = getComputerChoice();
+console.log(playerSelection)
+console.log(computerSelection)
+console.log(playRound(playerSelection, computerSelection));
